f318b2e6a450a81dbff5066eacbba54c59f9d745
Ramanpreet Chand <rchand595@gmail.com>
2017-07-01 12:17:31 -0400

    started writing larger programs

--- src/main/scala/WritingLargerPrograms.scala
+++ src/main/scala/WritingLargerPrograms.scala
@@ -1,4 +1,12 @@
 /**
   * Created by raman on 7/1/2017.
   */
+import doodle.core._
+import doodle.core.Image._
+import doodle.syntax._
+import doodle.jvm.Java2DFrame._
+import doodle.backend.StandardInterpreter._
 
+object WritingLargerPrograms {
+  (circle(100) fillColor Color.paleGoldenrod lineColor Color.indianRed).draw
+}
